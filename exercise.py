76e964f1ab13edfa588b3c6e413cb9abe8cb7076
Md Shahzad Alam <99706423+mdshahzadalam@users.noreply.github.com>
2022-09-04 10:41:51 +0530

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,51 @@
+
+
+from pygame import mixer
+from datetime import datetime
+from time import time
+
+def musiconloop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        input_of_user = input()
+        if input_of_user == stopper:
+            mixer.music.stop()
+            break
+
+def log_now(msg):
+    with open("mylogs.txt", "a") as f:
+        f.write(f"{msg} {datetime.now()}\n")
+
+if __name__ == '__main__':
+    # musiconloop("water.mp3", "stop")
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+    watersecs = 4
+    exsecs = 8
+    eyessecs = 12
+
+    while True:
+        if time() - init_water > watersecs:
+            print("Water Drinking time. Enter 'drank' to stop the alarm.")
+            musiconloop("water.mp3.mpeg", 'drank')
+            init_water = time()
+            log_now("Drank Water at")
+
+        if time() - init_eyes >eyessecs:
+            print("Eye exercise time. Enter 'doneeyes' to stop the alarm.")
+            musiconloop('eyes.mp3.mpeg', 'doneeyes')
+            init_eyes = time()
+            log_now("Eyes Relaxed at")
+
+        if time() - init_exercise > exsecs:
+            print("Physical Activity Time. Enter 'donephy' to stop the alarm.")
+            musiconloop('exercise.mp3.mpeg', 'donephy')
+            init_exercise = time()
+            log_now("Physical Activity done at")
+
+
+
+
